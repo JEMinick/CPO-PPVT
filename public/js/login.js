e61c2e21fe8ed2356c7de4661802b164f28a7425
Angel <angelpena619@gmail.com>
2021-06-23 21:31:47 -0700

    routing

--- public/js/login.js
+++ public/js/login.js
@@ -2,14 +2,14 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const username = document.querySelector('#name-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (username && password) {
+  if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -26,15 +26,15 @@ const signupFormHandler = async (event) => {
   event.preventDefault();
 
   const username = document.querySelector('#name-signup').value.trim();
-  // const email = document.querySelector('#email-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  console.log( "Attempting sign-up for: [" + JSON.stringify({ username, password }) + "]" );
+  console.log( "Attempting sign-up for: [" + JSON.stringify({ username, email, password }) + "]" );
 
-  if (username && password) {
+  if (username && password && email) {
     const response = await fetch('/api/users/newuser', {
       method: 'POST',
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username, password, email }),
       headers: { 'Content-Type': 'application/json' },
     });
 
